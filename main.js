83a328ee4adf9fbb7d11bffd74f2c3eba5ed8333
nicolasmanrique <nicolas@grupo-p.com>
2020-12-10 21:51:19 -0500

    Modal activo

--- main.js
+++ main.js
@@ -6,21 +6,34 @@ window.addEventListener("scroll", (e)=>
 
     if (position > 10 ) 
     {
-        header.querySelector("h3").classList.remove("text-dark")    
-        header.querySelector("h3").classList.add("text-white")  
+        header.querySelector("h4").classList.remove("text-dark")    
+        header.querySelector("h4").classList.add("text-white")  
         header.classList.add("opacityON")
         header.classList.remove("opacityOFF")
     }
     else
     {
-        header.querySelector("h3").classList.add("text-dark") 
-        header.querySelector("h3").classList.remove("text-white")
+        header.querySelector("h4").classList.add("text-dark") 
+        header.querySelector("h4").classList.remove("text-white")
         header.classList.add("opacityOFF") 
         header.classList.remove("opacityON")
     }
-
 })
 
 
+let abrir = document.getElementById("abrir"),
+    modal = document.getElementById("modal"),
+    cerrar = document.getElementById("cerrar")
+
+
+abrir.addEventListener("click", ()=> 
+{
+    modal.classList.remove("d-none")
+})
+
+cerrar.addEventListener("click", ()=> 
+{
+    modal.classList.add("d-none")
+})
 
 
