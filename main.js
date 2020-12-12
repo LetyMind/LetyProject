b01158d424cc2b2918cd377852f1db1ae23071cd
nicolasmanrique <nicolas@grupo-p.com>
2020-12-12 12:25:30 -0500

    Landing Terminada

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-const header = document.getElementById("header")
+const header = document.getElementById("header"),
+    abrirModal = document.getElementById("abrirModal")
+
+let windows = window.innerWidth 
 
 window.addEventListener("scroll", (e)=> 
 {
     let position = Math.round(window.scrollY) 
 
-    if (position > 10 ) 
-    {
-        header.querySelector("h4").classList.remove("text-dark")    
-        header.querySelector("h4").classList.add("text-white")  
-        header.classList.add("opacityON")
-        header.classList.remove("opacityOFF")
-    }
-    else
-    {
-        header.querySelector("h4").classList.add("text-dark") 
-        header.querySelector("h4").classList.remove("text-white")
-        header.classList.add("opacityOFF") 
-        header.classList.remove("opacityON")
-    }
+        if (position > 10) 
+        {
+            header.querySelector("h4").classList.remove("text-dark")
+            header.querySelector("h4").classList.add("text-white")
+            header.classList.add("opacityON")
+            header.classList.remove("opacityOFF")
+        }
+        else 
+        {
+            header.querySelector("h4").classList.add("text-dark")
+            header.querySelector("h4").classList.remove("text-white")
+            header.classList.add("opacityOFF")
+            header.classList.remove("opacityON")
+        }
+     
 })
 
 
@@ -35,5 +39,18 @@ cerrar.addEventListener("click", ()=>
 {
     modal.classList.add("d-none")
 })
+
+
+if ( windows < 768 ) 
+{
+    abrirModal.addEventListener("click", () => 
+    {
+        modal.classList.remove("d-none")
+    })   
+}
+ 
+
+
+
 
 
